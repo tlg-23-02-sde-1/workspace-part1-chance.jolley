14baca3ea5fcd9dafb21dec2dcd583e4998782a4
Chance Jolley <chance.jolley@tlgcohort.com>
2023-02-22 15:23:22 -0800

    10.1 complete

---
+++ Lab10.1-Television/src/com/entertainment/InvalidBrandException.java
@@ -0,0 +1,25 @@
+package com.entertainment;
+
+/*
+ *Application
+ */
+
+public class InvalidBrandException extends Exception {
+    public InvalidBrandException() {
+        super();
+    }
+
+    public InvalidBrandException(String message) {
+        super(message);
+    }
+
+    public InvalidBrandException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidBrandException(Throwable cause) {
+        super(cause);
+    }
+
+
+}
---
+++ Lab10.1-Television/src/com/entertainment/client/TelevisionClient.java
@@ -0,0 +1,52 @@
+package com.entertainment.client;
+
+import com.entertainment.DisplayType;
+import com.entertainment.InvalidBrandException;
+import com.entertainment.Television;
+
+/*
+ * Application "main-class" (class with a main() class).
+ * We'll create a few instances of Television and give them a basic test drive.
+ */
+class TelevisionClient {
+//   FIELDS or INSTANCE VARIABLES
+    public static void main(String[] args) {
+
+        System.out.println(Television.getInstanceCount() + " instances have been created.");
+        System.out.println();
+
+        try {
+            Television tv1 = new Television();
+            tv1.setBrand("Samsung");
+            tv1.setVolume(Television.MIN_VOLUME);
+
+            Television tv2 = new Television("Sony", Television.MAX_VOLUME, DisplayType.PLASMA);
+
+            Television tv3 = new Television("LG");
+            System.out.println();
+
+            tv1.turnOn();
+            tv2.turnOn();
+            tv3.turnOn();
+            System.out.println();
+
+            tv1.turnOff();
+            tv2.turnOff();
+            tv3.turnOff();
+            System.out.println();
+
+            System.out.println(tv1);
+            System.out.println(tv2);
+            System.out.println(tv3);
+            System.out.println();
+        } catch (InvalidBrandException e) {
+            System.out.println(e);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+
+        System.out.println(Television.getInstanceCount() + " instances have been created.");
+
+    }
+}
